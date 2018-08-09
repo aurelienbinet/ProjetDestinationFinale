d473e3b905d0edd63d2ac6c7243b91dbdf30b134
Alfred hitch cock <aurelien.binet34@gmail.com>
2018-08-09 12:42:51 +0200

    Ajout des Requests

--- src/test/java/test/App.java
+++ src/test/java/test/App.java
@@ -3,13 +3,18 @@ package test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import repositories.AeroportRepository;
 import repositories.ClientRepository;
 
 public class App {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		ClientRepository clientRepository = ctx.getBean(ClientRepository.class);
+		AeroportRepository aeroportRepository = ctx.getBean(AeroportRepository.class);
+		
+		aeroportRepository.findCustomByIdWithVolArrive((long) 100);
+		
+		
 
 	}
 
