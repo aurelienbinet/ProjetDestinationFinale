bfaaa895967b4a7e46762975f048bc48fd755bb8
Alfred hitch cock <aurelien.binet34@gmail.com>
2018-08-09 11:25:23 +0200

    Ajout des Repositories

---
+++ src/main/java/repositories/CompagnieAerienneVolRepository.java
@@ -0,0 +1,7 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompagnieAerienneVolRepository extends JpaRepository<CompagnieAerienneVol, CompagnieAerienneVolPK>{
+
+}
